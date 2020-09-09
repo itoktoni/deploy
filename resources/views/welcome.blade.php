61195435a46eaefc8d43557451ac60c8a902644a
Itok toni laksono <itok.toni@gmail.com>
2020-09-09 20:46:58 +0700

    update

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    itok toni laksono
                 </div>
 
                 <div class="links">
